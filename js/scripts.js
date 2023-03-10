bb7e4b13f3dba91d5343dfbc47a63cee15dcefe8
walterkarabin <walter.karabin@gmail.comclc>
2023-03-10 13:34:27 -0500

    add email to contact, update socials location

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,15 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+function CopyMe(TextToCopy) {
+    var TempText = document.createElement("input");
+    TempText.value = TextToCopy;
+    document.body.appendChild(TempText);
+    TempText.select();
+    
+    document.execCommand("copy");
+    document.body.removeChild(TempText);
+    
+    alert("Copied the text: " + TempText.value);
+  }
